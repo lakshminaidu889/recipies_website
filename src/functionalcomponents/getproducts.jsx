d008b4b0c51fce088ef8bfb138cbd624833a3c00
lakshminaidu093@gmail.com <lakshminaidu093@gmail.com>
2026-09-05 15:02:09 +0530

    backend deplyed api added

--- src/functionalcomponents/getproducts.jsx
+++ src/functionalcomponents/getproducts.jsx
@@ -4,7 +4,7 @@ function Getproducts() {
   const [products,setproducts]=useState([])
   //get recipies is a funtion which will fetch the data from the api and return the data in json format
   const get_recipes=async ()=>{
-  let response=await fetch('http://127.0.0.1:8000/recipes/')  
+  let response=await fetch('https://foodrecipes-backend-server.onrender.com/recipes/')  
   response =await response.json()
   setproducts(response.recipes)
   }
